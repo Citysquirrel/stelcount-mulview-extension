065037b7e8a425ae50ea2c2814c48e3d486af4e0
Citysquirrel <tok1324@naver.com>
2025-02-19 17:01:51 +0900

    content.js 롤백: content scripts 에서는 chrome 객체만 사용가능

--- src/content.js
+++ src/content.js
@@ -5,14 +5,14 @@
 // Copyright (c) 2024 jebibot
 // This file is licensed under the MIT License.
 // See the LICENSE file in the project root for more information.
-const browser = window.browser || window.chrome;
+// const browser = window.browser || window.chrome;
 
 try {
 	window.top.location.hostname;
 } catch {
 	if (window.location.hostname === "chzzk.naver.com") {
 		const script = document.createElement("script");
-		script.src = browser.runtime.getURL("src/chzzk.js");
+		script.src = chrome.runtime.getURL("src/chzzk.js");
 		script.onload = () => {
 			console.log("script loaded successfully");
 		};
